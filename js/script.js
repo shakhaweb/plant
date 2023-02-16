41de42b3c47b3ceb492aefa1161d788a768a6cac
shakhaweb <121448974+shakhaweb@users.noreply.github.com>
2023-02-16 19:37:03 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let burger = document.querySelector('#menu__toggle');
+let a = document.querySelector('.hamburger-menu')
+let b = document.querySelector('.active')
+burger.onclick=()=>{
+    a.classList.toggle('menu__box')
+}
+  
+  
